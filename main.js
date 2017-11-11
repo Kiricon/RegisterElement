38ade720b2d76dde921426ca3e11191ee3d90d3e
Dominic Valenciana <kiricon@live.com>
2017-11-10 21:44:20 -0700

    added registration

---
+++ main.js
@@ -0,0 +1,28 @@
+function buildTagName(name) {
+    return [...name].map( (x, i) => {
+        if( x < 'a') {
+            if(i !== 0) {
+                return `-${x.toLowerCase()}`;
+            }
+            return x.toLowerCase();
+        }
+        return x;
+    }).join('');
+}
+
+exports = (c) => {
+    const template = document.createElement('template');
+    template.innerHTML = c.prototype.template();
+    c.prototype.template = null;
+
+    class b extends c {
+        constructor() {
+            super();
+            this.attachShadow({mode: 'open'});
+            this.shadowRoot.appendChild(template.content.cloneNode(true));
+        }
+
+        template() {}
+    }
+    customElements.define(buildTagName(c.name), b);
+}
